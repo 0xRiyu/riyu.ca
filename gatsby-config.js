f037898b65e58391a827134edaca775da9a00792
Drew <Drew@riyu.ca>
2021-08-04 01:19:05 -0400

    Getting Started

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "RiyuDev",
+  },
+  plugins: ["gatsby-plugin-netlify-cms"],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const IndexPage = () => {
+  return (
+    <Layout pagetitle="Home Page">
+      <p>I'm making this using a tutorial neat</p>
+    </Layout>
+  );
+};
+
+export default IndexPage;
