7f73bc4e76a52ddc2d82dcf9deed4ed8c1bcd618
Resoona <cornfield_drew@yahoo.com>
2021-08-06 00:35:49 -0400

    Getting started with the blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "RiyuDev",
   },
-  plugins: ["gatsby-plugin-netlify-cms"],
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
-    <Layout pagetitle="Home Page">
+    <Layout pageTitle="Home Page">
       <p>I'm making this using a tutorial neat</p>
+      <StaticImage
+        alt="something something image"
+        src="..\images\cat.jpg"
+      />
     </Layout>
   );
 };
