ec31e66a4a0d286c1eeac0e36d918e7b57031d0f
Resoona <cornfield_drew@yahoo.com>
2021-08-12 00:10:23 -0400

    Stylize Journal page

--- src/pages/journal.js
+++ src/pages/journal.js
@@ -2,16 +2,38 @@ import * as React from "react";
 import { Link, graphql } from "gatsby";
 import Layout from "../components/layout";
 import webpageBanner from "../images/book-cart.jpg";
+import {
+  contentContainer,
+  articleContent,
+  blogEntryBg,
+  articleEntry,
+} from "../styles/content-container.module.css";
 
 const JournalPage = ({ data }) => {
   return (
     <Layout pageTitle="Journal" pageHeaderImage={webpageBanner}>
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>
-            <Link to={`${node.fields.slug}`}>{node.frontmatter.title}</Link>
-          </h2>
-          <p>Posted: {node.frontmatter.date}</p>
+          <div className={contentContainer}>
+            <div className={`${articleContent}`}>
+              <div className={blogEntryBg}>
+                <img
+                  src={
+                    require("../images/" + node.frontmatter.featuredImage)
+                      .default
+                  }
+                />
+              </div>
+              <div className={articleEntry}>
+                <h2>
+                  <Link to={`${node.fields.slug}`}>
+                    {node.frontmatter.title}
+                  </Link>
+                </h2>
+                <p style={{ lineHeight: 2 }}>Posted: {node.frontmatter.date}</p>
+              </div>
+            </div>
+          </div>
         </article>
       ))}
     </Layout>
@@ -25,6 +47,7 @@ export const query = graphql`
         frontmatter {
           date(formatString: "MMMM D, YYYY")
           title
+          featuredImage
         }
         fields {
           slug
