bcc919995a33be7a8c39744fcbc2aefddf0ee5c8
Resoona <cornfield_drew@yahoo.com>
2021-10-30 22:51:46 -0400

    Style cleanup

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import Layout from "../components/layout";
-import webpageBanner from "../images/quote-book-close.jpg";
+import webpageBanner from "../images/redtree.jpg";
 import {
   contentContainer,
   articleContent,
